56ba6cf2dd3f0a2a63d094ef4139c4e9332ede77
solayap2601 <solayap@unal.edu.co>
2022-11-08 01:50:44 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,24 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
 
     #
     # Inserte su código aquí
     #
+
+    df.dropna(axis=0, inplace=True)
+    df.monto_del_credito = df.monto_del_credito.str.strip("$")
+    df.monto_del_credito = df.monto_del_credito.str.replace(",","")
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+
+    for fila in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
+        df[fila] = df[fila].str.lower()
+        df[fila] = df[fila].str.replace('_', ' ')
+        df[fila] = df[fila].str.replace('-', ' ')
+
+    df.drop_duplicates(inplace=True)
 
     return df
